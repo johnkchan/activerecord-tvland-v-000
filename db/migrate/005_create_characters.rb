6dd74950824bf16f1f5aff8e8072f2c26ea71640
Jonathan Chan <johnkchan252@gmail.com>
2018-10-11 02:08:42 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :show_id
+      t.integer :show_id
     end
   end
 end
